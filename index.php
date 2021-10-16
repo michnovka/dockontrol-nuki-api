1e02ab87bdae30a36e31f14fcd7ef5693c2267c3
Tomas <michnovka@gmail.com>
2021-10-16 16:32:42 +0200

    Refactored to use Access class

--- index.php
+++ index.php
@@ -1,50 +1,44 @@
 <?php
 
+namespace Dockontrol\NukiAPI;
+
+use SQLite3;
+
 require_once dirname(__FILE__).'/libs/config.php';
+/** @var string $SQLITE_FILE */
+/** @var array $access_list */
+
 require_once dirname(__FILE__).'/libs/lib_totp.php';
 
 function APIError($message){
-    echo json_encode(array(
+    echo json_encode([
         'status' => 'error',
         'message' => $message
-    ));
+    ]);
     exit;
 }
 
-if(empty($access[$_GET['username']])){
+$access = null;
+
+if(!empty($access_list[$_GET['username']])){
+	$access = $access_list[$_GET['username']];
+}else{
 	APIError("Unauthorized");
 }
-if(!empty($access[$_GET['username']]['ip_whitelist']) && !in_array($_SERVER['REMOTE_ADDR'], $access[$_GET['username']]['ip_whitelist'])){
-	$access_granted = false;
 
-	if(!empty($access[$_GET['username']]['domain_whitelist'])){
-		foreach($access[$_GET['username']]['domain_whitelist'] as $domain){
-			$ips = gethostbynamel($domain);
-			
-			if(array_search($_SERVER['REMOTE_ADDR'], $ips)){
-				$access_granted = true;
-				break;
-			}
-		}
-	}
-	
-	if(!$access_granted)
-		APIError("Unauthorized IP");
+/** @var Access $access */
+
+if(!$access->CheckIPAndDomainWhitelist($_SERVER['REMOTE_ADDR'])){
+	APIError("Unauthorized");
 }
-
-$password1 = $access[$_GET['username']]['password1'];
-$password2 = $access[$_GET['username']]['password2'];
 
 $nonce = substr($_GET['nonce'], 0, 32);
 $totps = intval($_GET['totp1']).'|'.intval($_GET['totp2']);
 
 $db = new SQLite3($SQLITE_FILE);
 
-
 $db->exec('CREATE TABLE IF NOT EXISTS nonces(totps TEXT PRIMARY KEY, nonce TEXT, created_time INT);');
-
 $db->exec('DELETE FROM nonces WHERE created_time < '.(time()-3600));
-
 $nonce_used = $db->querySingle('SELECT 1 FROM nonces WHERE totps="'.SQLite3::escapeString($totps).'" AND nonce = "'.SQLite3::escapeString($nonce).'" LIMIT 1;') ? true : false;
 
 if($nonce_used){
@@ -53,28 +47,19 @@ if($nonce_used){
 
 $db->query('INSERT INTO nonces(totps, nonce, created_time) VALUES ("'.SQLite3::escapeString($totps).'","'.SQLite3::escapeString($nonce).'",'.time().');');
 
-// authenticate TOTP1 and TOTP2
-$secret1 = str_pad(GoogleAuthenticator::hex_to_base32(substr(hash('sha256', $password1),0,20)), 16, 'A', STR_PAD_LEFT).str_pad(GoogleAuthenticator::hex_to_base32(substr(hash('sha256', $nonce),0,10)), 8, 'A', STR_PAD_LEFT);
-$secret2 = str_pad(GoogleAuthenticator::hex_to_base32(substr(hash('sha256', $password2),0,20)), 16, 'A', STR_PAD_LEFT).str_pad(GoogleAuthenticator::hex_to_base32(substr(hash('sha256', $nonce),0,10)), 8, 'A', STR_PAD_LEFT);
-
-if(
-	empty($_GET['totp1']) || strlen($_GET['totp1']) != 6 ||
-	empty($_GET['totp2']) || strlen($_GET['totp2']) != 6 ||
-	!GoogleAuthenticator::check_totp($secret1, $_GET['totp1'])
-	|| !GoogleAuthenticator::check_totp($secret2, $_GET['totp2'])
-){
+if(!$access->CheckTOTPs($nonce, $_GET['totp1'], $_GET['totp2'])){
 	APIError("Unauthorized");
 }
 
-$nuki_ip = $access[$_GET['username']]['nuki_ip'];
-$nuki_id = $access[$_GET['username']]['nuki_id'];
-$nuki_port = $access[$_GET['username']]['nuki_port'];
-$nuki_token = $access[$_GET['username']]['nuki_token'];
-$device_type = $access[$_GET['username']]['device_type'];
+$nuki_ip = $access->getNukiIP();
+$nuki_id = $access->getNukiID();
+$nuki_port = $access->getNukiPort();
+$nuki_token = $access->getNukiToken();
+$device_type = $access->getDeviceType();
 
 $nuki_action = null;
 
-$is_sandbox = $access[$_GET['username']]['sandbox'];
+$is_sandbox = $access->isSandbox();
 
 switch($_GET['action']) {
 	case 'unlock':
---
+++ libs/Access.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace Dockontrol\NukiAPI;
+
+use GoogleAuthenticator;
+
+require_once dirname(__FILE__).'/lib_totp.php';
+
+class Access
+{
+	private string $password1;
+	private string $password2;
+	private string $nuki_ip;
+	private int $nuki_port;
+	private int $nuki_id;
+	private string $nuki_token;
+	private string $device_type;
+	private array $ip_whitelist;
+	private array $domain_whitelist;
+	private bool $sandbox = false;
+	/**
+	 * @return string
+	 */
+	public function getPassword1(): string
+	{
+		return $this->password1;
+	}
+	
+	/**
+	 * @param string $password1
+	 */
+	public function setPassword1(string $password1): void
+	{
+		$this->password1 = $password1;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getPassword2(): string
+	{
+		return $this->password2;
+	}
+	
+	/**
+	 * @param string $password2
+	 */
+	public function setPassword2(string $password2): void
+	{
+		$this->password2 = $password2;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getNukiIP(): string
+	{
+		return $this->nuki_ip;
+	}
+	
+	/**
+	 * @param string $nuki_ip
+	 */
+	public function setNukiIP(string $nuki_ip): void
+	{
+		$this->nuki_ip = $nuki_ip;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getNukiPort(): int
+	{
+		return $this->nuki_port;
+	}
+	
+	/**
+	 * @param int $nuki_port
+	 */
+	public function setNukiPort(int $nuki_port): void
+	{
+		$this->nuki_port = $nuki_port;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getNukiID(): int
+	{
+		return $this->nuki_id;
+	}
+	
+	/**
+	 * @param int $nuki_id
+	 */
+	public function setNukiID(int $nuki_id): void
+	{
+		$this->nuki_id = $nuki_id;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getNukiToken(): string
+	{
+		return $this->nuki_token;
+	}
+	
+	/**
+	 * @param string $nuki_token
+	 */
+	public function setNukiToken(string $nuki_token): void
+	{
+		$this->nuki_token = $nuki_token;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getDeviceType(): string
+	{
+		return $this->device_type;
+	}
+	
+	/**
+	 * @param string $device_type
+	 */
+	public function setDeviceType(string $device_type): void
+	{
+		$this->device_type = $device_type;
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function getIpWhitelist(): array
+	{
+		return $this->ip_whitelist;
+	}
+	
+	/**
+	 * @param array $ip_whitelist
+	 */
+	public function setIpWhitelist(array $ip_whitelist): void
+	{
+		$this->ip_whitelist = $ip_whitelist;
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function getDomainWhitelist(): array
+	{
+		return $this->domain_whitelist;
+	}
+	
+	/**
+	 * @param array $domain_whitelist
+	 */
+	public function setDomainWhitelist(array $domain_whitelist): void
+	{
+		$this->domain_whitelist = $domain_whitelist;
+	}
+	
+	public function isSandbox(): bool
+	{
+		return $this->sandbox;
+	}
+	
+	public function setSandbox(bool $sandbox): void
+	{
+		$this->sandbox = $sandbox;
+	}
+	
+	
+	public function __construct(string $password1, string $password2, string $nuki_ip, int $nuki_port, int $nuki_id, string $nuki_token, string $device_type = '0', array $ip_whitelist = [], array $domain_whitelist = [], $sandbox = false)
+	{
+		$this->password1 = $password1;
+		$this->password2 = $password2;
+		$this->nuki_ip = $nuki_ip;
+		$this->nuki_port = $nuki_port;
+		$this->nuki_id = $nuki_id;
+		$this->nuki_token = $nuki_token;
+		$this->device_type = $device_type;
+		$this->ip_whitelist = $ip_whitelist;
+		$this->domain_whitelist = $domain_whitelist;
+		$this->sandbox = $sandbox;
+	}
+	
+	public function CheckIPAndDomainWhitelist($ip): bool
+	{
+		$access_granted = true;
+		
+		if(!empty($this->getIpWhitelist()) && !in_array($ip, $this->getIpWhitelist())){
+			$access_granted = false;
+			
+			if(!empty($access[$_GET['username']]['domain_whitelist'])){
+				foreach($access[$_GET['username']]['domain_whitelist'] as $domain){
+					$ips = gethostbynamel($domain);
+					
+					if(array_search($ip, $ips)){
+						$access_granted = true;
+						break;
+					}
+				}
+			}
+			
+		}
+		
+		return $access_granted;
+	}
+	
+	public function CheckTOTPs($nonce, $totp1, $totp2): bool
+	{
+
+		// authenticate TOTP1 and TOTP2
+		$secret1 = str_pad(GoogleAuthenticator::hex_to_base32(substr(hash('sha256', $this->getPassword1()),0,20)), 16, 'A', STR_PAD_LEFT).str_pad(GoogleAuthenticator::hex_to_base32(substr(hash('sha256', $nonce),0,10)), 8, 'A', STR_PAD_LEFT);
+		$secret2 = str_pad(GoogleAuthenticator::hex_to_base32(substr(hash('sha256', $this->getPassword2()),0,20)), 16, 'A', STR_PAD_LEFT).str_pad(GoogleAuthenticator::hex_to_base32(substr(hash('sha256', $nonce),0,10)), 8, 'A', STR_PAD_LEFT);
+		
+		if(
+			empty($totp1) || strlen($totp1) != 6 ||
+			empty($totp2) || strlen($totp2) != 6 ||
+			!GoogleAuthenticator::check_totp($secret1, $totp1)
+			|| !GoogleAuthenticator::check_totp($secret2, $totp2)
+		){
+			return false;
+		}
+		
+		return true;
+	}
+	
+}
